99b81f2da846afa303c406993832f317ec1b37af
friendou <robert.dmc@hotmail.com>
2020-02-28 22:53:39 -0400

    Adding basic courses component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,8 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'courses',
+    loadChildren: () => import('./modules/courses/courses.module').then(m => m.CoursesModule)
+  },
+  {
+    path: '',
+    redirectTo: 'courses',
+    pathMatch: 'full'
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
