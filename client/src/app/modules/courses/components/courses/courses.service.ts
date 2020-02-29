ef8f7df43682a97c5a78a4a637a885a54ac55730
friendou <robert.dmc@hotmail.com>
2020-02-29 14:13:51 -0400

    added logic to use local storage

--- client/src/app/modules/courses/components/courses/courses.service.ts
+++ client/src/app/modules/courses/components/courses/courses.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ICourse } from '../../models/ICourses';
 import { Observable, of } from 'rxjs';
+import { cloneDeep } from 'lodash';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  courses: ICourse[] = [
+  private courses: ICourse[] = [
     {
       id: 1,
       name: 'Introduction to advertising',
@@ -51,13 +52,29 @@ export class CoursesService {
   constructor() {}
 
   getCourses(): Observable<ICourse[]> {
-    return of(this.courses);
+    const string = localStorage.getItem('a');
+    const json = JSON.parse(string);
+    this.courses = json;
+    return of(json);
   }
 
   putCourse(course: ICourse): Observable<ICourse> {
     const index = this.courses.findIndex((c) => c.id === course.id);
-    this.courses.splice(index, 1, course);
+    const courses = cloneDeep(this.courses);
+    courses[index] = course;
+    this.courses = courses;
+    localStorage.setItem('a',JSON.stringify(this.courses))
     return of(course);
+  }
+
+  postCourse(course: ICourse): Observable<ICourse> {
+    var courses = cloneDeep(this.courses);
+    const newCourse = cloneDeep(course)
+    newCourse.id = this.courses.length;
+    courses.push(newCourse);
+    this.courses = courses;
+    localStorage.setItem('a',JSON.stringify(this.courses))
+    return of(course)
   }
 
 }
