50a16ed353c50a5a267bd26375c2a05b127a823d
friendou <robert.dmc@hotmail.com>
2020-02-29 19:03:50 -0400

    Added textbook model

--- server/coursesapi/courses/models.py
+++ server/coursesapi/courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from djongo import models
 import uuid
 from django.contrib.auth.models import User
 
@@ -6,16 +7,27 @@ def generate_id():
     return 10
 
 # Create your models here.
+class Textbook(models.Model):
+    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return "{} - {}".format(self.author, self.title)
+
 class Course(models.Model):
+    _id = models.ObjectIdField()
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     courseId = models.IntegerField()
+    textbooks = models.ArrayField(
+        model_container=Textbook
+    )
     
     def __str__(self):
-        return "{} - {}".format(self.name, self.id)
+        return "{} - {}".format(self.name, self.courseId)
 
     def save(self, *args, **kwargs):
-        if len(self.courseId.strip(" "))==0:
+        if self.courseId==0:
             self.id = generate_id()
 
         super(Course, self).save(*args, **kwargs)
