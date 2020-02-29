a42bb61a7e6939deb8f953ada80fdc837d2eeba4
friendou <robert.dmc@hotmail.com>
2020-02-29 11:50:27 -0400

    Adding courses state to app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
+import { CoursesState } from './services/ngxs/courses/courses.state';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
+      NgxsModule.forRoot([CoursesState], {
+        developmentMode: !environment.production
+      }),
     BrowserAnimationsModule,
     MatToolbarModule
   ],
