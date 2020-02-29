85ae9bce89a540d4a484211e03be5150452f6133
friendou <robert.dmc@hotmail.com>
2020-02-29 11:52:39 -0400

    using store value to render courses

--- client/src/app/modules/courses/components/courses/courses.component.ts
+++ client/src/app/modules/courses/components/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CoursesService } from './courses.service';
 import { ICourse } from '../../models/ICourses';
-import { Observable } from 'rxjs';
+import { Observable, } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CoursesStateService } from 'src/app/services/ngxs/courses/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -12,10 +14,18 @@ export class CoursesComponent implements OnInit {
 
   courses$: Observable<ICourse[]>;
 
-  constructor(private coursesService: CoursesService) { }
+  constructor(
+    private coursesService: CoursesService,
+    private coursesStateService: CoursesStateService
+  ) { }
 
   ngOnInit(): void {
-    this.courses$ = this.coursesService.getCourses();
-  }
+    this.coursesService.getCourses()
+      .subscribe((data) => {
+        this.coursesStateService.setCourses(data);
+        this.coursesStateService.setBackupCourses(data);
+      });
 
+    this.courses$ = this.coursesStateService.courses$;
+  }
 }
