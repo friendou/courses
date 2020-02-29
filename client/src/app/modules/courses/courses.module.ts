cc6214cdb79a9e4a4b99dd272e1ee5f1ac27e416
friendou <robert.dmc@hotmail.com>
2020-02-29 12:33:29 -0400

    Adding modules

--- client/src/app/modules/courses/courses.module.ts
+++ client/src/app/modules/courses/courses.module.ts
@@ -6,15 +6,26 @@ import { CourseEditorComponent } from './components/course-editor/course-editor.
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+import { TextbookEditorComponent } from './components/textbook-editor/textbook-editor.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [CoursesComponent, CourseEditorComponent],
+  declarations: [CoursesComponent, CourseEditorComponent, TextbookEditorComponent],
   imports: [
     CommonModule,
     CoursesRoutingModule,
     MatExpansionModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    ReactiveFormsModule,
+    MatCardModule
   ]
 })
 export class CoursesModule { }
