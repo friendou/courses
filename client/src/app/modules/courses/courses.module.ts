c66f5081df69036f24735d22b5cae74fcd209ae6
friendou <robert.dmc@hotmail.com>
2020-02-29 00:20:12 -0400

    Importing modules

--- client/src/app/modules/courses/courses.module.ts
+++ client/src/app/modules/courses/courses.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import { CoursesComponent } from './components/courses/courses.component';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CourseEditorComponent } from './components/course-editor/course-editor.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [CoursesComponent, CourseEditorComponent],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class CoursesModule { }
