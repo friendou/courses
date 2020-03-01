5026f27133795cbd5261b2929b7af7b87ee272c6
friendou <robert.dmc@hotmail.com>
2020-02-29 21:54:54 -0400

    Fixed broken test

--- client/src/app/modules/courses/components/courses/courses.component.spec.ts
+++ client/src/app/modules/courses/components/courses/courses.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesComponent } from './courses.component';
+import { CoursesStateService } from 'src/app/services/ngxs/courses/courses.service';
+import { CoursesService } from './courses.service';
+import { of } from 'rxjs';
 
 describe('CoursesComponent', () => {
   let component: CoursesComponent;
   let fixture: ComponentFixture<CoursesComponent>;
+  let mockCourseStateService, mockCoursesService;
 
   beforeEach(async(() => {
+    mockCourseStateService = jasmine.createSpyObj('CourseStateService', [
+      'setCourses',
+      'setBackupCourses',
+      'addCourse'
+    ]);
+
+    mockCoursesService = jasmine.createSpyObj('CoursesService', [
+      'getCourses'
+    ])
+    mockCoursesService.getCourses.and.returnValue(of([]))
     TestBed.configureTestingModule({
-      declarations: [ CoursesComponent ]
+      declarations: [ CoursesComponent ],
+      providers: [
+        { provide: CoursesStateService, useValue:  mockCourseStateService },
+        { provide: CoursesService, useValue: mockCoursesService }
+      ]
     })
     .compileComponents();
   }));
