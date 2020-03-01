076a96bb721a0b0fc0a7d2047d1f6588aed380ed
friendou <robert.dmc@hotmail.com>
2020-02-29 20:19:46 -0400

    Adding vcors middleware

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config')
 
 // Import routes
 const courseRoutes = require('./routes/course');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/course', courseRoutes)
 
